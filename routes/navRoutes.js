d71dfdd3f20bf7143ff3260d497b9afde2294bd6
Wrestling341 <evgeniyzorich@mail.ru>
2021-07-15 13:30:59 +0300

    add: add routes for navigation

---
+++ routes/navRoutes.js
@@ -0,0 +1,22 @@
+export const ROUTES = [
+    {
+        name: 'HOME',
+        picture: 'https://i.imgur.com/jXuJMEh.png',
+        path: '/',
+    },
+    {
+        name: 'FAVORITES',
+        picture: 'https://i.imgur.com/6aVMkiT.png',
+        path: '/favorites',
+    },
+    {
+        name: 'SEARCH',
+        picture: 'https://i.imgur.com/2Y1rH4f.png',
+        path: '/search',
+    },
+    {
+        name: 'CART',
+        picture: 'https://i.imgur.com/fPFTWR4.png',
+        path: '/cart',
+    }
+]
