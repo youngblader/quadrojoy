c2abeac3e23b0f3d86bc37044184fc55dc9b139c
Wrestling341 <evgeniyzorich@mail.ru>
2021-07-15 13:32:29 +0300

    add: add screens in project

---
+++ screens/searchScreen/searchScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const SearchScreen = () => {
+    return (
+      <View style={styles.container}>
+        <Text>Search Screen</Text>
+      </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center', 
+    },
+});
+  
+
+export { SearchScreen };
